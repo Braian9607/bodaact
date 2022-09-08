75152264e685768d8a8f6bb35730c3f309d87274
yaz servin <yazz.serga.ys@gmail.com>
2022-09-08 15:13:41 -0500

    YAZ MYSQL

---
+++ src/conexion_mysql.py
@@ -0,0 +1,9 @@
+import mysql.connector
+
+conexion = mysql.connector.connect(user='root', 
+    password = '1234',
+    host='localhost',
+    database='boda',
+    port = '3306')
+
+print(conexion)
