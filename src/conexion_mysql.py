7871c93a9d6cca1eb1c330397b134fc8c58b2efe
Braian9607 <braian_rodriguez9607@gmail.com>
2022-10-09 21:05:38 -0500

    sabrosamente

--- src/conexion_mysql.py
+++ src/conexion_mysql.py
@@ -1,9 +1,9 @@
 import mysql.connector
 
-conexion = mysql.connector.connect(user='root', 
-    password = '1234',
-    host='localhost',
-    database='boda',
-    port = '3306')
+conexion = mysql.connector.connect(user='root',
+          password = '1234',
+          host='localhost',
+          database='boda',
+          port='3306')
 
 print(conexion)
