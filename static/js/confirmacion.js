415ebc2b1d29eb19285ca686acf94cd61c3b6e57
yaz servin <yazz.serga.ys@gmail.com>
2022-10-13 20:04:54 -0500

    prueba

--- static/js/confirmacion.js
+++ static/js/confirmacion.js
@@ -1,16 +1,16 @@
-$(document).ready(function(){
-	$('.btncode').on('click',function(){
-        document.getElementById('div_buscar').style.display = 'none';
-        var codigo = $('#codigo').val();
-        console.log(codigo);
-        req = $.ajax({
-            url : '/confirmacion/buscar',
-            type : 'POST',
-            data : { codigo : codigo}
-        });
-        req.done(function(data) {
-            console.log(data.familia);
-            document.getElementById('div_confirmacion').style.display = '';
-        });
-	});
-});
+// $(document).ready(function(){
+// 	$('.btncode').on('click',function(){
+//         document.getElementById('div_buscar').style.display = 'none';
+//         var codigo = $('#codigo').val();
+//         console.log(codigo);
+//         req = $.ajax({
+//             url : '/confirmacion/buscar',
+//             type : 'POST',
+//             data : { codigo : codigo}
+//         });
+//         req.done(function(data) {
+//             console.log(data.familia);
+//             document.getElementById('div_confirmacion').style.display = '';
+//         });
+// 	});
+// });
