426803b779e0fdc54b5fde56b3e1d894084602dd
yaz servin <yazz.serga.ys@gmail.com>
2022-10-19 00:00:26 -0500

    check

--- form.py
+++ form.py
@@ -1,4 +1,6 @@
 #from mysql import Cursor
+from pickle import APPEND
+from tkinter.font import families
 from flask import Flask, render_template, url_for, request, jsonify
 from flask_mysqldb import MySQL
 import mysql.connector
@@ -33,16 +35,55 @@ def buscar_inv():
     #nombreUser  = request.form['nombreUser']
     codigo = request.form['codigo']
     print(codigo)
+   
+    ## Buscar familia 
     cur = mysql.connection.cursor()
-    cur.execute('''SELECT u.familia, i.nombre
+    cur.execute('''SELECT u.familia
         FROM usuario u
-        left join invitados i on u.clave_inv = i.clave_inv 
         WHERE u.clave_inv = %s''', [codigo])
     data_fam = cur.fetchall()
     cur.close()
+    familia = data_fam[0][0]
+    print('familia',familia)
 
-    print(data_fam)
-    return jsonify({'result' : 'success', 'familia' : data_fam}) 
+    ## Buscar Invitados 
+    cur = mysql.connection.cursor()
+    cur.execute('''SELECT i.nombre
+        FROM usuario u
+        left join invitados i on u.clave_inv = i.clave_inv 
+        WHERE u.clave_inv = %s''', [codigo])
+    data_inv = cur.fetchall()
+    cur.close()
+    invi = []
+    for i in range(len(data_inv)):
+        for j in range(len(data_inv[i])):
+            invi.append(data_inv[i][j])
+    invitados = sorted(invi)
+    print('Invitados',invitados)
+
+    ## Número de invitados adultos
+    cur = mysql.connection.cursor()
+    cur.execute('''SELECT count(distinct i.nombre)
+        FROM usuario u
+        left join invitados i on u.clave_inv = i.clave_inv 
+        WHERE u.clave_inv = %s and i.tipo_persona = "Adulto" ''', [codigo] )
+    cantidad_a = cur.fetchall()
+    cur.close()
+    print(cantidad_a[0][0])
+    
+   ## Número de invitados niños
+    cur = mysql.connection.cursor()
+    cur.execute('''SELECT count(distinct i.nombre)
+        FROM usuario u
+        left join invitados i on u.clave_inv = i.clave_inv 
+        WHERE u.clave_inv = %s and i.tipo_persona = "Niño" ''', [codigo] )
+    cantidad_n = cur.fetchall()
+    cur.close()
+    
+    print(cantidad_n[0][0])
+
+    return jsonify({'result' : 'success', 'familia' : familia, 'invitados' : invitados,
+        'cantidad_a': cantidad_a, 'cantidad_n': cantidad_n }) 
 
 
 if __name__ == "__main__":
--- static/js/confirmacion.js
+++ static/js/confirmacion.js
@@ -9,8 +9,39 @@ $(document).ready(function(){
             data : { codigo : codigo}
         });
         req.done(function(data) {
+            console.log(data);
             console.log(data.familia);
+            console.log(data.invitados);
             document.getElementById('div_confirmacion').style.display = '';
+            fs = data.familia
+            let numb = "Familia "+fs;
+            document.getElementById("familia").innerHTML = numb;
+            var inv = data.invitados
+            console.log('array', inv)
+
+           
+
+            let myTable= "<div>";
+                  
+
+            for (let i = 0; i < inv.length; i++) {
+
+                myTable+="<div class='row ng-scope' ng-repeat='invitado in adultos' style=''>"
+                myTable+="<div class='col-md-1 col-sm-1'>"
+                myTable+="<input type='checkbox' id='asistencia'>"
+                myTable+="</div>"
+                myTable+="<div class='col-md-11 col-sm-11'>"
+                myTable+="<h6 class='guest-name ng-binding'>" + inv[i] + "</h6>"
+                myTable+="</div>"
+                myTable+="</div>"
+            }
+      
+            myTable+="</div>";
+            document.getElementById('tablePrint').innerHTML = myTable;
+            
         });
 	});
+
 });
+
+
