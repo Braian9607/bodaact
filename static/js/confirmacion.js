36d6b939fc9ecdbd039e30a580eac507c413581d
yaz servin <yazz.serga.ys@gmail.com>
2022-10-21 14:38:24 -0500

    yazz

--- static/js/confirmacion.js
+++ static/js/confirmacion.js
@@ -43,40 +43,46 @@ $(document).ready(function(){
             counpers+="</div>"
             document.getElementById('countpersonas').innerHTML = counpers
 
-            let myTable= "<div>";
-              
-            // console.log( inv.length)
-            // for (let i = 0; i <  inv.length; i++) {
-            //     console.log('for',  inv[i].nombre)
-            //     myTable+="<div class='row ng-scope' ng-repeat='invitado in adultos' style=''>"
-            //     myTable+="<div class='col-md-1 col-sm-1' id='select_check'>"
-            //     myTable+="<input type='checkbox' value='"+ inv[i].id +"' class='asistencia_check'>"
-            //     myTable+="</div>"
-            //     myTable+="<div class='col-md-11 col-sm-11'>"
-            //     myTable+="<p class='' style='color: white; margin: auto;'>" + inv[i].nombre + "</p>"
-            //     myTable+="</div>"
-            //     myTable+="</div>"
-            // }
+            
+            $('.btn_continuar').on('click',function(){
+                document.getElementById('div_cconfi').style.display = '';
+                document.getElementById('div_confirmacion').style.display = 'none';
+                console.log( filteredAdul.length)
+                let myTable= "<div>";
+
+                for (let i = 0; i <  filteredAdul.length; i++) {
+                    console.log('for',  filteredAdul[i].nombre)
+                    myTable+="<div class='row ng-scope' ng-repeat='invitado in adultos' style=''>"
+                    myTable+="<div class='col-md-1 col-sm-1' id='select_check'>"
+                    myTable+="<input type='checkbox' value='"+ filteredAdul[i].id +"' class='asistencia_check'>"
+                    myTable+="</div>"
+                    myTable+="<div class='col-md-11 col-sm-11'>"
+                    myTable+="<p class='' style='color: white; margin: auto;'>" + filteredAdul[i].nombre + "</p>"
+                    myTable+="</div>"
+                    myTable+="</div>"
+                }
       
-            // myTable+="</div>";
-            // document.getElementById('tablePrint').innerHTML = myTable;
+                myTable+="</div>";
+                document.getElementById('tablePrint').innerHTML = myTable;
+            });   
+            
             
         });
-        $('.btn_continuar').on('click',function(){
-            var selected = '';    
-            $('#select_check input[type=checkbox]').each(function(){
-                if (this.checked) {
-                    selected += $(this).val()+', ';
-                }
-            }); 
+        // $('.btn_continuar').on('click',function(){
+        //     var selected = '';    
+        //     $('#select_check input[type=checkbox]').each(function(){
+        //         if (this.checked) {
+        //             selected += $(this).val()+', ';
+        //         }
+        //     }); 
     
-            if (selected != '') 
-                alert('Has seleccionado: '+selected);  
-            else
-                alert('Debes seleccionar al menos una opción.');
+        //     if (selected != '') 
+        //         alert('Has seleccionado: '+selected);  
+        //     else
+        //         alert('Debes seleccionar al menos una opción.');
     
-            return false;
-        });  
+        //     return false;
+        // });  
 	});
     
     
